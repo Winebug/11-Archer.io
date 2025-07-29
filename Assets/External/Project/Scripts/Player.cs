34e757ae000eba9a4e16c402183cfa741c6273e8
minjeparkk <zone8572@gmail.com>
2025-07-29 15:23:54 +0900

    Feat : 플레이어 이동 구현

--- Assets/External/Project/Scripts/Player.cs
+++ Assets/External/Project/Scripts/Player.cs
@@ -5,12 +5,9 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class PlayerController : UnitController
 {
-    private Camera camera; // 마우스 위치를 월드 좌표로 변환하기 위한 메인 카메라 참조
-
     protected override void Start()
     {
         base.Start();
-        camera = Camera.main;
     }
 
     protected override void HandleAction()
@@ -19,22 +16,15 @@ public class PlayerController : UnitController
         float horizontal = Input.GetAxisRaw("Horizontal"); // A/D 또는 ←/→
         float vertical = Input.GetAxisRaw("Vertical"); // W/S 또는 ↑/↓
 
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonDown("Horizontal");
+        bool vUp = Input.GetButtonDown("Vertical");
+
         // 방향 벡터 정규화 (대각선일 때 속도 보정)
         movementDirection = new Vector2(horizontal, vertical).normalized;
 
-        // 마우스 위치를 화면 좌표 → 월드 좌표로 변환
-        Vector2 mousePosition = Input.mousePosition;
-        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
-        lookDirection = (worldPos - (Vector2)transform.position);
+        lookDirection = new Vector2(horizontal,0);
 
-        // 현재 위치로부터 마우스 위치까지의 방향 계산
-        if (lookDirection.magnitude < .9f)
-        {
-            lookDirection = Vector2.zero;
-        }
-        else
-        {
-            lookDirection = lookDirection.normalized;
-        }
     }
 }
--- Assets/External/Project/Scripts/UnitController.cs
+++ Assets/External/Project/Scripts/UnitController.cs
@@ -5,6 +5,8 @@ public class UnitController : MonoBehaviour
 {
     protected Rigidbody2D _rigidbody; // 이동을 위한 물리 컴포넌트
 
+    protected AnimationHandler animationHandler;
+
     [SerializeField] private SpriteRenderer characterRenderer; // 좌우 반전을 위한 렌더러
     [SerializeField] private Transform weaponPivot; // 무기를 회전시킬 기준 위치
 
@@ -20,6 +22,7 @@ public class UnitController : MonoBehaviour
     protected virtual void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        animationHandler = GetComponent<AnimationHandler>();
     }
 
     protected virtual void Start()
@@ -42,11 +45,14 @@ public class UnitController : MonoBehaviour
         }
     }
 
+    // 캐릭터, 적 핸들액션 오버라이드 받기
     protected virtual void HandleAction()
     {
 
     }
 
+
+    // 캐릭터 이동
     private void Movment(Vector2 direction)
     {
         direction = direction * 5; // 이동 속도
@@ -60,8 +66,12 @@ public class UnitController : MonoBehaviour
 
         // 실제 물리 이동
         _rigidbody.velocity = direction;
+
+        // 이동 애니메이션 처리
+        animationHandler.Move(direction);
     }
 
+    // 스프라이트 좌우 반전(캐릭,무기)
     private void Rotate(Vector2 direction)
     {
         float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
@@ -77,6 +87,7 @@ public class UnitController : MonoBehaviour
         }
     }
 
+    // 넉백
     public void ApplyKnockback(Transform other, float power, float duration)
     {
         knockbackDuration = duration;
