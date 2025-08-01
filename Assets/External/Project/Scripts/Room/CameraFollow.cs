9f6b77320c13767c344836647b4f8f00bed99ddd
Winebug <ydjoo0410@gmail.com>
2025-08-01 20:20:48 +0900

    chore: chore
    chore: chore

--- Assets/External/Project/Scripts/Room/CameraFollow.cs
+++ Assets/External/Project/Scripts/Room/CameraFollow.cs
@@ -2,9 +2,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;  // Player
-    public float smoothSpeed = 5f;  // 부드러운 추적 속도
-    public Vector3 offset;  // 카메라 오프셋 (Player 위쪽이나 살짝 뒤쪽)
+    public Transform target;
+    public float smoothSpeed = 5f;
+    public Vector3 offset;
+
+    [Header("카메라 범위")]
+    public float minY = 0f;        // 첫 방 시작 Y
+    public float roomHeight = 20f; // 방 간격
+    public float maxY;             // 동적으로 변경됨
+
+    private float fixedX = -12.23f;
+
+    private int currentRoomIndex = 0; // 현재 방 인덱스 (0부터 시작)
 
     void LateUpdate()
     {
@@ -12,6 +21,15 @@ public class CameraFollow : MonoBehaviour
 
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
-        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
+
+        // X는 고정, Y는 Clamp
+        float clampedY = Mathf.Clamp(smoothedPosition.y, minY, maxY);
+        transform.position = new Vector3(fixedX, clampedY, transform.position.z);
     }
+    public void EnterRoom(int roomIndex)
+    {
+        currentRoomIndex = roomIndex;
+        maxY = minY + (currentRoomIndex + 1) * roomHeight;
+    }
+
 }
