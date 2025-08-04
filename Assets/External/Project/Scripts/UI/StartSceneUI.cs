d102d6262b5052f338fda150cbfa77ff662586e9
Insung Park <sleepwithoutadream@gmail.com>
2025-08-04 21:41:38 +0900

    sampleScene

--- Assets/External/Project/Scripts/UI/StartSceneUI.cs
+++ Assets/External/Project/Scripts/UI/StartSceneUI.cs
@@ -26,8 +26,8 @@ public class StartSceneUI : MonoBehaviour
     private void OnStartClicked()
     {
         UIManager.Instance.StopBGM();
-        //SceneManager.LoadScene("SampleScene"); //나중에 씬이름 변경시 이 코드도 변경
-        SceneManager.LoadScene("SkillTestTemp2"); //나중에 씬이름 변경시 이 코드도 변경
+        SceneManager.LoadScene("SampleScene"); //나중에 씬이름 변경시 이 코드도 변경
+        //SceneManager.LoadScene("SkillTestTemp2"); //나중에 씬이름 변경시 이 코드도 변경
     }
 
     private void OnExitClicked()
